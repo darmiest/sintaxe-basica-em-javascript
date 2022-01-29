993c2e64cd7590861b92b6939b51ebd3f5d53035
Daiana Momberg de Andrade <52114937+darmiest@users.noreply.github.com>
2022-01-29 13:38:16 -0300

    aula 1 e 2

---
+++ aula 2/script.js
@@ -0,0 +1,109 @@
+// tipos primititvos
+
+//boolean 
+/*var trueOrFalse = false;
+console.log(trueOrFalse);
+
+//saber o tipo da variavel
+console.log(typeof(trueOrFalse));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'naruto';
+console.log(typeof(nome));
+
+// function
+var funcao = function(){}
+console.log(typeof(funcao));
+
+//como declarar
+var variavel = 'naruto';
+variavel = 'sasuke';
+console.log(variavel);
+
+let variavel2 = 'naruto';
+variavel2 = 'sasuke'
+console.log(variavel2)
+
+const constante = 'naruto';
+constante = 'kakashi'
+console.log(constante);*/
+
+/* var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = 'local';
+    console.log(escopoLocalInterno);
+}
+
+escopoLocal(); */
+
+/* //são palavras reservadas, não é possivel declarar
+var var
+var let
+var const 
+var function */
+  
+/* // atribuição
+var atribuição = 'diana';
+
+//comparação
+var comparacao = "0" == 0;
+console.log(comparacao);
+
+//comparacao identica
+var comparacaoidentica = '0' === 0;
+console.log(comparacaoidentica) */
+
+/* // adicao
+var adicao = 1 + 1;
+console.log(adicao);
+
+//subtracao
+var subtracao = 2 - 1;
+console.log(subtracao);
+
+//multiplicação
+var multiplicacao = 2* 3
+console.log(multiplicacao)
+
+//divisao real
+var divisaoReal = 6 / 2;
+console.log(divisaoReal);
+
+//divisao inteira
+var divisaoInteira = 5 % 2;
+console.log(divisaoInteira);
+
+// potenciação
+var potenciacao = 2 ** 10;
+console.log(potenciacao) */
+
+/* // maior que
+var maiorQue = 7 > 5;
+console.log(maiorQue);
+
+// menor que
+var menorQue = 7 < 5;
+console.log(menorQue);
+
+// maior ou igual que
+var maiorOuIgual = 5 >= 2;
+console.log(maiorQue); 
+
+// menor ou igual que
+var menorOuIgual = 5 <= 2;
+console.log(menorOuIgual); */
+
+var e = true && false
+console.log(e)
+
+var ou  = true || false
+console.log(ou)
+
+var nao = !true;
+console.log(nao)
