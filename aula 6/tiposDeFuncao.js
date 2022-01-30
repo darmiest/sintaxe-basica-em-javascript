eb474b04fe5fa6a29e23f1d07cc6c5676424eac0
Daiana Momberg de Andrade <52114937+darmiest@users.noreply.github.com>
2022-01-29 21:47:48 -0300

    aula 6 ok

---
+++ aula 6/tiposDeFuncao.js
@@ -0,0 +1,23 @@
+ // funções declarativas
+/* function funcao(){
+    console.log('Sou uma mensagem de uma função declarativa')
+}
+funcao();
+ */
+// expressões de funções com nomeação
+/* var funcao = function funcao(){
+    console.log('Sou uma mensagem de função de expressão!')
+}
+funcao() */
+
+// expressões de funções sem nomeação
+/* var funcao = function(){
+    console.log('Sou uma mensagem de função de expressão!')
+}
+funcao() */
+
+// arrow function: sintaxe curta
+/* var funcao = () => {
+    console.log('Sou uma arrow function!')
+}
+funcao(); */
